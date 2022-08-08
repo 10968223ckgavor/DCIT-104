71cf9fb7b2613044998a911ba210625aaf8f977c
10968223ckgavor <103081685+10968223ckgavor@users.noreply.github.com>
2022-08-08 16:32:09 +0000

    Add files via upload
    Done by myself

---
+++ main dcit 104.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+int isPrimeNumber(int k) {
+   bool isPrime = true;
+
+   for(int i = 2; i <= k/2; i++) {
+      if (k%i == 0)
+      {
+         isPrime = false;
+    num     break;
+      } 
+      cout<<"sum of true/number of true"
+   }
+
+   return isPrime;
+}
